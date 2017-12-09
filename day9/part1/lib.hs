b9e40bfa8600ddce04085cdfa1be38e0d2f90952
Per Rovegård <per@rovegard.se>
2017-12-09 15:18:24 +0100

    Making day 9 part 1 more idiomatic

--- day9/part1/lib.hs
+++ day9/part1/lib.hs
@@ -1,46 +1,33 @@
 module Lib where
-import Data.Maybe
-import Data.List
+import Data.Stack
 
 newtype Group = Group { score :: Int } deriving (Show, Eq)
 
-newtype Stack = Stack [Group] deriving (Show)
-
 data Memory = Memory
-              { stck :: Stack
+              { stck :: Stack Group
               , groups :: [Group]
               } deriving (Show)
 
-stackNew = Stack []
-stackPush (Stack gs) g = Stack $ g : gs
-stackPeek (Stack []) = Nothing
-stackPeek (Stack gs) = Just $ head gs
-stackPop (Stack []) = Nothing
-stackPop (Stack gs) = Just (Stack $ tail gs, head gs)
-
 dropGarbage :: String -> String
 dropGarbage "" = ""
-dropGarbage stream = case head stream of
-    '<' -> dropGarbage $ tail stream
-    '!' -> dropGarbage $ drop 2 stream
-    '>' -> tail stream
-    _   -> dropGarbage $ tail stream
+dropGarbage ('<':xs)   = dropGarbage xs
+dropGarbage ('!':_:xs) = dropGarbage xs
+dropGarbage ('>':xs)   = xs
+dropGarbage (_:xs)     = dropGarbage xs
 
 newGroup stack = case stackPeek stack of
     Just (Group s) -> stackPush stack $ Group $ s + 1
     Nothing -> stackPush stack $ Group 1
 
 findGroupsRec :: Memory -> String -> Memory
-findGroupsRec mem "" = mem
-findGroupsRec (Memory stack done) stream = case head stream of
-    '{' -> findGroupsRec (Memory (newGroup stack) done) $ tail stream
-    '}' -> case stackPop stack of
-        Just (s, g) -> findGroupsRec (Memory s (g : done)) $ tail stream
-        Nothing -> error "empty stack"
-    ',' -> findGroupsRec (Memory stack done) (tail stream)
-    _   -> findGroupsRec (Memory stack done) (dropGarbage stream)
+findGroupsRec mem ""                       = mem
+findGroupsRec (Memory stack done) ('{':xs) = findGroupsRec (Memory (newGroup stack) done) xs
+findGroupsRec mem (',':xs)                 = findGroupsRec mem xs
+findGroupsRec (Memory stack done) ('}':xs) = case stackPop stack of
+    Just (s, g) -> findGroupsRec (Memory s (g : done)) xs
+    Nothing -> error "empty stack"
+findGroupsRec mem stream                   = findGroupsRec mem $ dropGarbage stream
 
 findGroups :: String -> [Group]
-findGroups stream = do
-    let mem = findGroupsRec (Memory stackNew []) stream
-    groups mem
+findGroups stream = groups $ findGroupsRec emptyMem stream 
+    where emptyMem = Memory stackNew []
