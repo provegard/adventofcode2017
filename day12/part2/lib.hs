d4cd0bb965781430ba60be1deb29543b2a30bc60
Per Rovegård <per@rovegard.se>
2017-12-13 08:42:26 +0100

    Tidying day 12 part 2

--- day12/part2/lib.hs
+++ day12/part2/lib.hs
@@ -1,7 +1,5 @@
 module Lib where
-import Data.Foldable
 import Data.Graph
-import Data.Maybe
 
 toInt :: String -> Int
 toInt x = read x :: Int
@@ -20,19 +18,12 @@ findEdges line = do
     map (\a -> (a, x1Num)) restNum
 
 bounds :: [(Vertex, Vertex)] -> (Vertex, Vertex)
-bounds theEdges = do
-    let allV = map fst theEdges ++ map snd theEdges
-    (minimum allV, maximum allV)
+bounds theEdges = let allV = map fst theEdges ++ map snd theEdges
+                  in (minimum allV, maximum allV)
 
 parse :: [String] -> Graph
 parse theLines = let allEdges = concatMap findEdges theLines
                  in buildG (bounds allEdges) allEdges
 
--- groupSize :: Int -> [String] -> Int
--- groupSize node theLines = do
---     let g = parse theLines
---     length $ reachable g node
-
 numberOfGroups :: [String] -> Int
-numberOfGroups theLines = let g = parse theLines
-                          in length $ components g
+numberOfGroups theLines = length $ components $ parse theLines
