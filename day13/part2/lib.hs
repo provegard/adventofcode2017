0bcfda58fe0f2b87ef1ca4708287f72bdceffa1d
Per Rovegård <per@rovegard.se>
2017-12-15 23:23:30 +0100

    Refactoring day 13 part 2
    Introduction of bang pattern decreased the running time from
    34 seconds to 5 seconds.

--- day13/part2/lib.hs
+++ day13/part2/lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Lib where
 import Data.Foldable
 import Data.Maybe
@@ -9,10 +10,6 @@ toInt x = read x :: Int
 data Layer = Layer { scannerRange :: Int, scannerPosition :: Int, scannerDirection :: Int } deriving (Show, Eq)
 
 type Layers = [(Int, Layer)]
-
-data World = World { worldLayers :: Layers, packetPos :: Int, caught :: Bool, maxDepth :: Int }
-
-type Time = Int
 
 newLayer :: Int -> Layer
 newLayer range = Layer range 0 1
@@ -33,36 +30,32 @@ maxDepthOf layers = maximum $ map fst layers
 
 advanceLayers :: Layers -> Layers
 advanceLayers = map moveScannerTup
-    where moveScannerTup (depth, layer) = (depth, moveScanner layer)
+    where moveScannerTup (depth, !layer) = (depth, moveScanner layer)
 
 -- generate an infinite sequence of layers (layer configurations)
 layersGenerator :: Layers -> [Layers]
-layersGenerator layers = layers : layersGenerator (advanceLayers layers)
+layersGenerator layers = layers : (layersGenerator $! advanceLayers layers)
 
-isCaughtIn :: Layers -> Int -> Bool
-isCaughtIn layers maxDepth = do
-    let world = World layers (-1) False maxDepth
-    any caught (generateWorlds world)
+isCaughtIn :: [Layers] -> Int -> Bool
+isCaughtIn layerList maxDepth = isCaught layerList (-1)
     where
-        generateWorlds :: World -> [World]
-        generateWorlds world@(World layers packetPos _ maxDepth) = world : nextWorlds
-            where
-                nextWorlds
-                    | packetPos > maxDepth = []
-                    | otherwise = do
-                        let newPos = packetPos + 1
-                        let caught = isCaught newPos
-                        let newLayers = advanceLayers layers
-                        let newWorld = World newLayers newPos caught maxDepth
-                        world : generateWorlds newWorld
-                    where
-                        isCaught aPos = case lookup aPos layers of
-                            Just l | scannerPosition l == 0 -> True
-                            _                               -> False
+        isCaught :: [Layers] -> Int -> Bool
+        isCaught (layers:remaining) pos
+            | pos > maxDepth = False
+            | otherwise = do
+                let newPos = pos + 1
+                let caught = checkCaught newPos
+                caught || isCaught remaining newPos
+                where
+                    checkCaught aPos = case lookup aPos layers of
+                        Just l | scannerPosition l == 0 -> True
+                        _                               -> False
 
 findDelay :: [Layers] -> Int -> Int -> Int
-findDelay (layers:remaining) delay maxD epth = 
-    if not (isCaughtIn layers maxDepth) then delay else findDelay remaining (delay + 1) maxDepth
+findDelay layersList delay maxDepth = do
+    let passed = not $! isCaughtIn layersList maxDepth
+    if passed then delay else next
+    where next = findDelay (tail layersList) (delay + 1) maxDepth
 
 getMinDelay :: [String] -> Int
 getMinDelay lines = do
