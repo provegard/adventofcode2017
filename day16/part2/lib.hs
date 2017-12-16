a59612f548aa61d0b975ce1350a0be2996275234
Per Rovegård <per@rovegard.se>
2017-12-16 11:33:39 +0100

    Day 16 part 2 array WIP

--- day16/part2/lib.hs
+++ day16/part2/lib.hs
@@ -2,12 +2,17 @@
 module Lib where
 import Data.Char
 import Data.List.Split
-import qualified Data.Sequence as Seq
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.Unboxed
+import Data.STRef
 
 data DanceMove = Swap Int
                | Exchange Int Int
-               | Partner String String deriving (Eq, Show)
-type Dancers = Seq.Seq String
+               | Partner Char Char deriving (Eq, Show)
+--type Dancers = V.MVector s Char
+--type Dancers = UArray Int Char
 
 toInt x = read x :: Int
 
@@ -22,34 +27,56 @@ parseInput str = do
         toDanceMove ('s':rest) = Swap $ toInt rest
         toDanceMove ('p':rest) = do
             let (p1,p2) = firstTwo $ splitOn "/" rest
-            Partner p1 p2
+            Partner (head p1) (head p2)
         toDanceMove ('x':rest) = do
             let (s1,s2) = firstTwo $ splitOn "/" rest
             Exchange (toInt s1) (toInt s2)
         toDanceMove other = error ("Unknown dance move " ++ other)
 
-createDancers :: Int -> Dancers
-createDancers num = let dancer i = chr (97 + i) : ""
-                     in Seq.fromList $ map dancer [0..(num-1)]
+createDancers :: Int -> UArray Int Char
+createDancers num = let dancer i = chr (97 + i)
+                    in listArray (0,num-1) $ map dancer [0..(num-1)]
 
-applyDanceMove :: Dancers -> DanceMove -> Dancers
+applyDanceMove :: STUArray s Int Char -> DanceMove -> ST s ()
 applyDanceMove dancers move = case move of
-    Swap num       -> let (a,b) = Seq.splitAt (Seq.length dancers - num) dancers
-                      in (Seq.><) b a
+    Swap num       -> do
+        (_, upper) <- getBounds dancers
+        let arrLen = upper + 1
+        let startIdx = arrLen - num
+        let newIdx i = (startIdx + i) `mod` arrLen
+        temp <- mapIndices (0,upper) newIdx dancers -- can we avoid this allocation?
+        forM_ [0..upper] $ \i -> do
+            d <- readArray temp i
+            writeArray dancers i d
+        return ()
     Exchange i1 i2 -> do
-        let d1 = Seq.index dancers i1
-        let d2 = Seq.index dancers i2
-        let r1 = Seq.update i1 d2 dancers
-        Seq.update i2 d1 r1
+        d1 <- readArray dancers i1
+        d2 <- readArray dancers i2
+        writeArray dancers i1 d2
+        writeArray dancers i2 d1
+        return ()
     Partner p1 p2  -> do
-        let ix = Seq.findIndicesL (\d -> d == p1 || d == p2) dancers
-        let (i1,i2) = firstTwo ix
-        applyDanceMove dancers (Exchange i1 i2)
+        -- let ix = Seq.findIndicesL (\d -> d == p1 || d == p2) dancers
+        -- let (i1,i2) = firstTwo ix
+        (_, upper) <- getBounds dancers
+        i1 <- newSTRef (-1)
+        i2 <- newSTRef (-1)
+        forM_ [0..upper] $ \i -> do
+            d <- readArray dancers i
+            if d == p1 then
+                writeSTRef i1 i
+            else when (d == p2) $ writeSTRef i2 i
+            -- TODO: break if both written
+        ii1 <- readSTRef i1
+        ii2 <- readSTRef i2
+        applyDanceMove dancers (Exchange ii1 ii2)
+        return ()
 
 finalOrderMulti :: Int -> Int -> String -> String
 finalOrderMulti repeatCount dancerCount str = do
-    let moves = parseInput str
-    let dancers = createDancers dancerCount
-    let oneDance ds = foldl applyDanceMove ds moves
-    let positions = foldl (\ !ds _ -> oneDance ds) dancers [1..repeatCount]
-    concat positions
+    let moves = parseInput str -- a list
+    let positionsArr = runSTUArray $ do
+        dancers <- thaw $ createDancers dancerCount
+        forM_ [1..repeatCount] $ \_ -> forM_ moves $ \m -> applyDanceMove dancers m
+        return dancers
+    elems positionsArr
--- day16/part2/test.hs
+++ day16/part2/test.hs
@@ -1,7 +1,11 @@
 module Lib.Tests where
 import Test.Hspec
 import Lib
-import qualified Data.Sequence as Seq
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.Unboxed
+import Data.STRef
 
 main = hspec $ do
     describe "parseInput" $ do
@@ -10,16 +14,31 @@ main = hspec $ do
         it "parses xA/B" $ do
             (parseInput "x5/6") `shouldBe` [(Exchange 5 6)]
         it "parses pA/B" $ do
-            (parseInput "pp/e") `shouldBe` [(Partner "p" "e")]
+            (parseInput "pp/e") `shouldBe` [(Partner 'p' 'e')]
         it "splits on comma" $ do
-            (parseInput "s5,pe/b") `shouldBe` [(Swap 5), (Partner "e" "b")]
+            (parseInput "s5,pe/b") `shouldBe` [(Swap 5), (Partner 'e' 'b')]
+    describe "createDancers" $ do
+        it "creates them" $ do
+            (createDancers 5) `shouldBe` (listArray (0,4) "abcde")
     describe "applyDanceMove" $ do
         it "applies Swap" $ do
-            (applyDanceMove (createDancers 5) (Swap 1)) `shouldBe` (Seq.fromList ["e","a","b","c","d"])
+            let retVal = runSTUArray $ do
+                dan <- thaw $ createDancers 5
+                applyDanceMove dan (Swap 1)
+                return dan
+            retVal `shouldBe` (listArray (0,4) "eabcd")
         it "applies Exchange" $ do
-            (applyDanceMove (createDancers 5) (Exchange 3 4)) `shouldBe` (Seq.fromList ["a","b","c","e","d"])
+            let retVal = runSTUArray $ do
+                dan <- thaw $ createDancers 5
+                applyDanceMove dan (Exchange 3 4)
+                return dan
+            retVal `shouldBe` (listArray (0,4) "abced")
         it "applies Partner" $ do
-            (applyDanceMove (createDancers 5) (Partner "e" "b")) `shouldBe` (Seq.fromList ["a","e","c","d","b"])
+            let retVal = runSTUArray $ do
+                dan <- thaw $ createDancers 5
+                applyDanceMove dan (Partner 'e' 'b')
+                return dan
+            retVal `shouldBe` (listArray (0,4) "aecdb")
     describe "finalOrderMulti" $ do
         it "works for the example" $ do
             (finalOrderMulti 2 5 "s1,x3/4,pe/b") `shouldBe` "ceadb"
