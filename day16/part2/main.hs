190a3b8e7f574d198829cd4d05fcee1dc9081431
Per Rovegård <per@rovegard.se>
2017-12-16 09:34:55 +0100

    Day 16 part 2 WIP

---
+++ day16/part2/main.hs
@@ -0,0 +1,10 @@
+module Main where
+import Lib
+
+toInt x = read x :: Int
+
+main = do
+  str <- getLine
+  --let res = finalOrderMulti 1000000000 16 str
+  let res = finalOrderMulti 1000 16 str
+  putStrLn res
