ca3d734f6d3b9a148d018645febb8fc85b69ed63
Per Rovegård <per@rovegard.se>
2017-12-17 03:07:37 +0100

    day 16 part 2 mutable array WIP

--- day16/part2/lib.hs
+++ day16/part2/lib.hs
@@ -7,24 +7,53 @@ import Control.Monad.ST
 import Data.Array.ST
 import Data.Array.Unboxed
 import Data.STRef
+import Data.Array.MArray
+import Debug.Trace
+import Data.Array.Base (unsafeRead, unsafeWrite, unsafeAt)
 
-data DanceMove = Swap Int
+data DanceMove = Spin Int
                | Exchange Int Int
                | Partner Char Char deriving (Eq, Show)
 --type Dancers = V.MVector s Char
 --type Dancers = UArray Int Char
+
+-- note: First is a single-elem array
+data Dancers s = Dancers (STRef s Int) (STUArray s Int Char) (STUArray s Int Int) Int
 
 toInt x = read x :: Int
 
 firstTwo :: [a] -> (a, a)
 firstTwo ss = (head ss, head $ tail ss) -- TODO: pattern matching ??
 
+dancersToString :: Dancers s -> ST s String
+dancersToString (Dancers i dancers indices _) = do
+    idx <- readSTRef i
+    el <- getElems dancers
+    -- is <- getElems indices
+    let (d,e) = splitAt idx el
+    let str1 = e ++ d
+    -- let charAt i = el!!i
+    -- let (a,b) = splitAt idx is
+    -- let orderedIndices = b ++ a
+    -- let str2 = map intToChar (b ++ a)
+    -- return $ check str1 str2
+    -- let str3 = map charAt orderedIndices
+    -- return str3
+    return str1
+    -- where 
+    --     intToChar i = chr (i + 97)
+    --     check s1 s2 = do
+    --         if s1 /= s2 then
+    --             error ("Mismatch: " ++ s1 ++ " != " ++ s2)
+    --         else
+    --             s2
+
 parseInput :: String -> [DanceMove]
 parseInput str = do
     let moves = splitOn "," str
     map toDanceMove moves
     where
-        toDanceMove ('s':rest) = Swap $ toInt rest
+        toDanceMove ('s':rest) = Spin $ toInt rest
         toDanceMove ('p':rest) = do
             let (p1,p2) = firstTwo $ splitOn "/" rest
             Partner (head p1) (head p2)
@@ -33,50 +62,112 @@ parseInput str = do
             Exchange (toInt s1) (toInt s2)
         toDanceMove other = error ("Unknown dance move " ++ other)
 
-createDancers :: Int -> UArray Int Char
-createDancers num = let dancer i = chr (97 + i)
-                    in listArray (0,num-1) $ map dancer [0..(num-1)]
+createDancers :: Int -> ST s (Dancers s)
+createDancers num = do
+    indices <- thaw (listArray (0,num-1) [0..(num-1)] :: UArray Int Int)
+    dancers <- thaw (listArray (0,num-1) $ map dancer [0..(num-1)] :: UArray Int Char)
+    i <- newSTRef 0
+    return $ Dancers i dancers indices num
+    where dancer i = chr (97 + i)
 
-applyDanceMove :: STUArray s Int Char -> DanceMove -> ST s ()
-applyDanceMove dancers move = case move of
-    Swap num       -> do
-        (_, upper) <- getBounds dancers
-        let arrLen = upper + 1
+swapInt :: STUArray s Int Int -> Int -> Int -> ST s ()
+swapInt arr i1 i2 = do
+    d1 <- readArray arr i1
+    d2 <- readArray arr i2
+    writeArray arr i1 d2
+    writeArray arr i2 d1
+    return ()
+-- TODO: WTF? How do I write one function for this?? type X = Int | Char ??
+swapChar :: STUArray s Int Char -> Int -> Int -> ST s ()
+swapChar arr i1 i2 = do
+    d1 <- readArray arr i1
+    d2 <- readArray arr i2
+    writeArray arr i1 d2
+    writeArray arr i2 d1
+    return ()
+    
+
+realIdx :: Dancers s -> Int -> ST s Int
+realIdx (Dancers si s _ len) i2 = do
+    startIdx <- readSTRef si
+    return $ (startIdx + i2) `mod` len
+
+virtIdx :: Dancers s -> Int -> ST s Int
+virtIdx (Dancers si s _ len) i2 = do
+    startIdx <- readSTRef si
+    return $ (i2 - startIdx) `mod` len
+                    
+applyDanceMove :: Dancers s -> DanceMove -> ST s ()
+applyDanceMove d@(Dancers si dancers indices arrLen) move = case move of
+    Spin num       -> do
         let startIdx = arrLen - num
-        let newIdx i = (startIdx + i) `mod` arrLen
-        temp <- mapIndices (0,upper) newIdx dancers -- can we avoid this allocation?
-        forM_ [0..upper] $ \i -> do
-            d <- readArray temp i
-            writeArray dancers i d
+        newIdx <- realIdx d startIdx
+        writeSTRef si newIdx
         return ()
     Exchange i1 i2 -> do
-        d1 <- readArray dancers i1
-        d2 <- readArray dancers i2
-        writeArray dancers i1 d2
-        writeArray dancers i2 d1
+        -- calculate the real array indices (given the index offset)
+        ri1 <- realIdx d i1
+        ri2 <- realIdx d i2
+        -- find che characters, so we can find the slots
+        p1 <- readArray dancers ri1
+        p2 <- readArray dancers ri2
+        let slotOfP1 = charToInt p1
+        let slotOfP2 = charToInt p2
+        
+        swapChar dancers ri1 ri2
+        -- swapInt indices ri1 ri2
+        swapInt indices slotOfP1 slotOfP2
         return ()
     Partner p1 p2  -> do
-        -- let ix = Seq.findIndicesL (\d -> d == p1 || d == p2) dancers
-        -- let (i1,i2) = firstTwo ix
-        (_, upper) <- getBounds dancers
-        i1 <- newSTRef (-1)
-        i2 <- newSTRef (-1)
-        forM_ [0..upper] $ \i -> do
-            d <- readArray dancers i
-            if d == p1 then
-                writeSTRef i1 i
-            else when (d == p2) $ writeSTRef i2 i
-            -- TODO: break if both written
-        ii1 <- readSTRef i1
-        ii2 <- readSTRef i2
-        applyDanceMove dancers (Exchange ii1 ii2)
+        -- calculate slot numbers - in each slot, we have the index of the corresponding dancer
+        let slotOfP1 = charToInt p1
+        let slotOfP2 = charToInt p2
+        -- find the indices stored in the slots
+        realIndexOfP1 <- readArray indices slotOfP1
+        realIndexOfP2 <- readArray indices slotOfP2
+        -- swap the dancers
+        swapChar dancers realIndexOfP1 realIndexOfP2
+        -- swap contents of the slots
+        swapInt indices slotOfP1 slotOfP2
+
+        -- figure out "virtual" indices (i.e. what )
+        -- startIndex <- readSTRef si
+        -- let virtIdx x = (x - startIndex) `mod` arrLen
+        -- let v1 = virtIdx ix1
+        -- let v2 = virtIdx ix2
+        -- -- swap
+        -- -- swapChar dancers ix1 ix2
+        -- -- swapInt indices ix1 ix2
+        -- applyDanceMove d (Exchange v1 v2)
         return ()
+    where 
+        charToInt c = ord c - 97
+
+debugDancers :: Dancers s -> DanceMove -> ST s String
+debugDancers (Dancers si dancers indices len) move = do
+    idx <- readSTRef si
+    ds <- getElems dancers
+    is <- getElems indices
+    let oks = map (isDancerOk ds is) [0..(len-1)]
+    let nok = False `elem` oks
+    let okstr = if nok then " NOK" else "    "
+    return $ show idx ++ " " ++ show ds ++ " " ++ show is ++ okstr ++ " ==> " ++ show move
+    where
+        isDancerOk ds is i = do
+            let expChar = chr (97 + i)
+            let indexOfChar = is!!i
+            let actChar = ds!!indexOfChar
+            expChar == actChar
+
 
 finalOrderMulti :: Int -> Int -> String -> String
 finalOrderMulti repeatCount dancerCount str = do
     let moves = parseInput str -- a list
-    let positionsArr = runSTUArray $ do
-        dancers <- thaw $ createDancers dancerCount
-        forM_ [1..repeatCount] $ \_ -> forM_ moves $ \m -> applyDanceMove dancers m
-        return dancers
-    elems positionsArr
+    runST $ do
+        dancers <- createDancers dancerCount
+        forM_ [1..repeatCount] $ \_ -> do
+            forM_ moves $ \m -> do
+                -- str <- debugDancers dancers m
+                -- applyDanceMove (trace str dancers) m
+                applyDanceMove dancers m
+        dancersToString dancers
--- day16/part2/test.hs
+++ day16/part2/test.hs
@@ -7,38 +7,66 @@ import Data.Array.ST
 import Data.Array.Unboxed
 import Data.STRef
 
+dance5 :: [DanceMove] -> String
+dance5 moves = do
+    let str = runST $ do
+        dancers <- createDancers 5
+        forM_ moves $ \m -> applyDanceMove dancers m
+        dancersToString dancers
+    str
+
 main = hspec $ do
     describe "parseInput" $ do
         it "parses sX" $ do
-            (parseInput "s5") `shouldBe` [(Swap 5)]
+            (parseInput "s5") `shouldBe` [(Spin 5)]
         it "parses xA/B" $ do
             (parseInput "x5/6") `shouldBe` [(Exchange 5 6)]
         it "parses pA/B" $ do
             (parseInput "pp/e") `shouldBe` [(Partner 'p' 'e')]
         it "splits on comma" $ do
-            (parseInput "s5,pe/b") `shouldBe` [(Swap 5), (Partner 'e' 'b')]
+            (parseInput "s5,pe/b") `shouldBe` [(Spin 5), (Partner 'e' 'b')]
     describe "createDancers" $ do
         it "creates them" $ do
-            (createDancers 5) `shouldBe` (listArray (0,4) "abcde")
+            let str = runST $ createDancers 5 >>= dancersToString
+            str `shouldBe` "abcde"
     describe "applyDanceMove" $ do
-        it "applies Swap" $ do
-            let retVal = runSTUArray $ do
-                dan <- thaw $ createDancers 5
-                applyDanceMove dan (Swap 1)
-                return dan
-            retVal `shouldBe` (listArray (0,4) "eabcd")
+        it "applies Spin" $ do
+            let moves = [Spin 1]
+            let retVal = dance5 moves
+            retVal `shouldBe` "eabcd"
         it "applies Exchange" $ do
-            let retVal = runSTUArray $ do
-                dan <- thaw $ createDancers 5
-                applyDanceMove dan (Exchange 3 4)
-                return dan
-            retVal `shouldBe` (listArray (0,4) "abced")
+            let moves = [Exchange 3 4]
+            let retVal = dance5 moves
+            retVal `shouldBe` "abced"
         it "applies Partner" $ do
-            let retVal = runSTUArray $ do
-                dan <- thaw $ createDancers 5
-                applyDanceMove dan (Partner 'e' 'b')
-                return dan
-            retVal `shouldBe` (listArray (0,4) "aecdb")
+            let moves = [Partner 'e' 'b']
+            let retVal = dance5 moves
+            retVal `shouldBe` "aecdb"
+        it "applies Exchange after Spin" $ do
+            let moves = [Spin 1, Exchange 0 1]
+            let retVal = dance5 moves
+            retVal `shouldBe` "aebcd"
+        it "applies Partner after Spin" $ do
+            let moves = [Spin 1, Partner 'b' 'd']
+            let retVal = dance5 moves
+            retVal `shouldBe` "eadcb"
+        it "applies Partner after Exchange" $ do
+            let moves = [Exchange 3 4, Partner 'd' 'b']
+            let retVal = dance5 moves
+            retVal `shouldBe` "adceb"
+        it "applies two Spins" $ do
+            let moves = [Spin 1, Spin 1]
+            let retVal = dance5 moves
+            retVal `shouldBe` "deabc"
+        it "applies two Partners" $ do
+            let moves = [Partner 'a' 'b', Partner 'b' 'c']
+            let retVal = dance5 moves
+            retVal `shouldBe` "cabde"
     describe "finalOrderMulti" $ do
-        it "works for the example" $ do
-            (finalOrderMulti 2 5 "s1,x3/4,pe/b") `shouldBe` "ceadb"
+        it "works for the example, 1 iteration only" $ do
+            (finalOrderMulti 1 5 "s1,x3/4,pe/b") `shouldBe` "baedc"
+        -- it "works for the example, 2 iterations" $ do
+        --     (finalOrderMulti 2 5 "s1,x3/4,pe/b") `shouldBe` "ceadb"
+        it "works for the example, debug" $ do
+            (finalOrderMulti 1 5 "s1,x3/4,pe/b,s1,x3/4,pe/b") `shouldBe` "ceadb"
+    
