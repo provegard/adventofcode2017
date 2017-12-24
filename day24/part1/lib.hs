ccb30a2d0f1e686692a9b675fb8c1e72fff09bb0
Per Rovegård <per@rovegard.se>
2017-12-24 09:00:09 +0100

    Day 24 part 1, more WIP (ComponentMap)

--- day24/part1/lib.hs
+++ day24/part1/lib.hs
@@ -12,6 +12,24 @@ data Component = Component Int Int deriving (Eq, Show, Ord)
 
 newtype Bridge = Bridge [Component] deriving (Eq, Show)
 
+newtype ComponentMap = ComponentMap (Map.Map Int [Component])
+
+createComponentMap :: [Component] -> ComponentMap
+createComponentMap components = do
+    let prefixed = concatMap (\c@(Component p1 p2) -> [(p1, c), (p2, c)]) components
+    ComponentMap $ sortAndGroup prefixed
+    where
+        sortAndGroup assocs = Map.fromListWith (++) [(k, [v]) | (k, v) <- assocs]
+
+findMatchingComponents :: Component -> ComponentMap -> [Component]
+findMatchingComponents c@(Component a b) (ComponentMap cmap) = do
+    let al = fromMaybe [] $ Map.lookup a cmap
+    let bl = fromMaybe [] $ Map.lookup b cmap
+    delete c (nub (al ++ bl))
+
+findStartComponents :: ComponentMap -> [Component]
+findStartComponents (ComponentMap cm) = fromMaybe [] $ Map.lookup 0 cm
+
 strength :: Bridge -> Int
 strength (Bridge ports) = let compStrength (Component a b) = a + b
                           in sum $ map compStrength ports 
@@ -30,32 +48,30 @@ createComponents = map parseLine
 fitTogether :: Component -> Component -> Bool
 fitTogether (Component p1a p1b) (Component p2a p2b) = p1a == p2a || p1a == p2b || p1b == p2a || p1b == p2b
 
-build' :: [Component] -> [[Component]]
-build' !components = do
-    let (starters, rest) = partition isStarter components
-    let restSet = Set.fromList rest
-    --concatMap (\s -> [s] : buildFrom (Set.singleton s) s rest) starters
-    concatMap (\s -> [s] : buildFrom (Set.singleton s) [s] s (Set.delete s restSet)) starters
+build' :: ComponentMap -> [[Component]]
+build' compMap = do
+    -- let (starters, rest) = partition isStarter components
+    -- let restSet = Set.fromList rest
+    let starters = findStartComponents compMap
+    concatMap (\s -> buildFrom (Set.singleton s) [s] s) starters
     where
         isStarter (Component a b) = a == 0 || b == 0
-        buildFrom :: Set.Set Component -> [Component] -> Component -> Set.Set Component -> [[Component]]
-        buildFrom !used !previousList previous !othersSet = do
+        buildFrom :: Set.Set Component -> [Component] -> Component -> [[Component]]
+        buildFrom !used !previousList previous = do
             -- a connector is ok if it fits with the previous and hasn't been used
-            --let others = Set.toList othersSet
-            let candidateSet = Set.filter (\c -> fitTogether previous c && not (Set.member c used)) othersSet
-            let candidates = Set.toList candidateSet
-            concatMap (\c -> do
-                let compList = previousList ++ [c]
-                compList : buildFrom (Set.insert c used) compList c (Set.delete c othersSet)) candidates
+            let matching = findMatchingComponents previous compMap
+            let candidates = filter (\c -> not (Set.member c used) && not (isStarter c)) matching
+            if null candidates then [previousList] else concatMap (\c -> do
+                    --let compList = previousList ++ [c]
+                    let compList = c : previousList -- prepend is faster I think, and order doesn't matter
+                    buildFrom (Set.insert c used) compList c) candidates
 
 build :: [Component] -> [Bridge]
-build components = map Bridge $ build' components
+build components = map Bridge $ build' (createComponentMap components)
 
 findStrongest :: [String] -> (Int, Bridge)
 findStrongest strings = do
     let components = createComponents strings
     let bridges = build components
     let b = maximumBy (\a b -> compare (strength a) (strength b)) bridges
-    -- let strengths = map strength bridges
-    -- maximum strengths
     (strength b, b)
--- day24/part1/test.hs
+++ day24/part1/test.hs
@@ -7,19 +7,19 @@ main = hspec $ do
     describe "parseLint" $ do
         it "parses" $ do
             (parseLine "1/2") `shouldBe` (Component 1 2)
-    describe "build'" $ do
+    describe "build" $ do
         it "builds a single port" $ do
             let ports = [Component 0 2]
-            let actual = build' ports
-            actual `shouldBe` [[Component 0 2]]
+            let actual = build ports
+            actual `shouldBe` [Bridge [Component 0 2]]
         it "builds from two matching ports" $ do
             let ports = [Component 0 2, Component 2 3]
-            let actual = build' ports
-            actual `shouldBe` [[Component 0 2], [Component 0 2, Component 2 3]]
+            let actual = build ports
+            actual `shouldBe` [Bridge [Component 2 3, Component 0 2]]
         it "builds from two start components" $ do
             let ports = [Component 0 2, Component 0 4]
-            let actual = build' ports
-            actual `shouldBe` [[Component 0 2], [Component 0 4]]
+            let actual = build ports
+            actual `shouldBe` [Bridge [Component 0 4], Bridge [Component 0 2]]
     describe "findStrongest" $ do
         it "works for the example" $ do
             let strings = ["0/2", "2/2", "2/3", "3/4", "3/5", "0/1", "10/1", "9/10"]
