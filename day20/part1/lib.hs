88cc4076b469bd0aa5ecd8d50fd7bf6193c853c2
Per Rovegård <per@rovegard.se>
2018-01-01 15:09:37 +0100

    Day 20 part 1, different solution
    Find the particle with least acceleration, if same
    find the one that is closest to begin with.

--- day20/part1/lib.hs
+++ day20/part1/lib.hs
@@ -1,20 +1,12 @@
-{-# LANGUAGE BangPatterns #-}
 module Lib where
-import Data.Char
-import Data.Maybe
-import Debug.Trace
 import Data.Foldable
 import Data.List.Split
 import Data.List
 import Text.Read
 
-
 data Triple = Triple { tx :: Int, ty :: Int, tz :: Int } deriving (Show, Eq)
-data Particle = Particle { position :: Triple, velocity :: Triple, acceleration :: Triple } deriving (Show, Eq)
-newtype ParticleSystem = ParticleSystem [Particle]
+data Particle = Particle { position :: Triple, acceleration :: Triple } deriving (Show, Eq)
 
--- toInt x = fromMaybe (error ("Not a number: " ++ show x)) (readMaybe x :: Maybe Int)
-    
 zipWithIndex :: [a] -> [(Int, a)]
 zipWithIndex = zip [0..]
 
@@ -24,47 +16,24 @@ parseInputLine str = do
     let numbers = flatMap id (map tryParse parts)
     let threes = chunksOf 3 numbers
     let triples = map toTriple threes
-    Particle (head triples) (triples!!1) (triples!!2)
+    Particle (head triples) (triples!!2)
     where
         tryParse x = readMaybe x :: Maybe Int
         flatMap f = concatMap (toList . f)
         toTriple (a:b:c:_) = Triple a b c
 
-manhattan :: Particle -> Int
-manhattan part = do
-    let pos = position part
-    sum $ map abs [tx pos, ty pos, tz pos]
+manhattan :: Triple -> Int
+manhattan (Triple x y z) = sum $ map abs [x, y, z]
 
-addTrip :: Triple -> Triple -> Triple
-addTrip t1 t2 = do
-    let x' = tx t1 + tx t2
-    let y' = ty t1 + ty t2
-    let z' = tz t1 + tz t2
-    Triple x' y' z'
-
-tick :: Particle -> Particle
-tick part = do
-    let v' = addTrip (velocity part) (acceleration part)
-    let p' = addTrip (position part) v' -- TODO: correct with v' rather than old velocity?
-    Particle p' v' (acceleration part)
-
-tickSystem :: ParticleSystem -> ParticleSystem
-tickSystem (ParticleSystem particles) = ParticleSystem (map tick particles)
-
-longRun' :: ParticleSystem -> Int -> Int
-longRun' sys upper = do
-    -- brute force
-    let futureSystems = drop (upper - 1) (systems sys)
-    let (ParticleSystem parts) = head futureSystems
-    let indexed = zipWithIndex parts
-    let sorted = sortOn mh indexed
+indexOfClosest :: [Particle] -> Int
+indexOfClosest particles = do
+    let sorted = sortBy comp $ zipWithIndex particles
     (fst . head) sorted
     where
-        systems s = s : (systems . tickSystem) s
-        mh (_,p) = manhattan p
-
+        comp (_, a) (_, b) = let x = compare (accel a) (accel b)
+                             in if x == EQ then compare (pos a) (pos b) else x
+        accel = manhattan . acceleration
+        pos   = manhattan . position
+    
 longRun :: [String] -> Int -> Int
-longRun strParticles upper = do
-    let parts = map parseInputLine strParticles
-    let sys = ParticleSystem parts
-    longRun' sys upper
+longRun strParticles upper = indexOfClosest $ map parseInputLine strParticles
