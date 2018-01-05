71f691a98bf96bbde3f33a727b8804e4d283e600
Per Rovegård <per@rovegard.se>
2018-01-05 13:29:33 +0100

    Faster day 13 part 2, only keep track of when scanners are at pos 0

--- day13/part2/lib.hs
+++ day13/part2/lib.hs
@@ -7,18 +7,15 @@ import Data.List.Split
 toInt :: String -> Int
 toInt x = read x :: Int
 
-data Layer = Layer { scannerRange :: Int, scannerPosition :: Int, scannerDirection :: Int } deriving (Show, Eq)
+newtype Layer = Layer { scannerRange :: Int } deriving (Show, Eq)
 
 type Layers = [(Int, Layer)]
 
 newLayer :: Int -> Layer
-newLayer range = Layer range 0 1
+newLayer = Layer
 
-moveScanner :: Layer -> Layer
-moveScanner (Layer sr sp sd)
-    | sp == 0 && sd == -1       = Layer sr 1 1
-    | sp == (sr - 1) && sd == 1 = Layer sr (sr - 2) (-1)
-    | otherwise                 = Layer sr (sp + sd) sd
+scannerAt0 :: Layer -> Int -> Bool
+scannerAt0 (Layer sr) time = 0 == time `mod` (2 * sr - 2)
 
 readLayers :: [String] -> Layers
 readLayers = map createLayer
@@ -28,37 +25,28 @@ readLayers = map createLayer
 maxDepthOf :: Layers -> Int
 maxDepthOf layers = maximum $ map fst layers
 
-advanceLayers :: Layers -> Layers
-advanceLayers = map moveScannerTup
-    where moveScannerTup (depth, !layer) = (depth, moveScanner layer)
-
--- generate an infinite sequence of layers (layer configurations)
-layersGenerator :: Layers -> [Layers]
-layersGenerator layers = layers : (layersGenerator $! advanceLayers layers)
-
-isCaughtIn :: [Layers] -> Int -> Bool
-isCaughtIn layerList maxDepth = isCaught layerList (-1)
+isCaughtIn :: Layers -> Int -> Int -> Bool
+isCaughtIn layers delay maxDepth = isCaught delay (-1)
     where
-        isCaught :: [Layers] -> Int -> Bool
-        isCaught (layers:remaining) pos
+        isCaught :: Int -> Int -> Bool
+        isCaught delay pos
             | pos > maxDepth = False
             | otherwise = do
                 let newPos = pos + 1
-                let caught = checkCaught newPos
-                caught || isCaught remaining newPos
+                let caught = checkCaught newPos delay
+                caught || isCaught (delay + 1) newPos
                 where
-                    checkCaught aPos = case lookup aPos layers of
-                        Just l | scannerPosition l == 0 -> True
-                        _                               -> False
+                    checkCaught aPos aDelay = case lookup aPos layers of
+                        Just l -> scannerAt0 l aDelay
+                        _      -> False
 
-findDelay :: [Layers] -> Int -> Int -> Int
-findDelay layersList delay maxDepth = do
-    let passed = not $! isCaughtIn layersList maxDepth
+findDelay :: Layers -> Int -> Int -> Int
+findDelay layers delay maxDepth = do
+    let passed = not $! isCaughtIn layers delay maxDepth
     if passed then delay else next
-    where next = findDelay (tail layersList) (delay + 1) maxDepth
+    where next = findDelay layers (delay + 1) maxDepth
 
 getMinDelay :: [String] -> Int
 getMinDelay lines = do
     let layers = readLayers lines
-    let layersList = layersGenerator layers
-    findDelay layersList 0 (maxDepthOf layers)
+    findDelay layers 0 (maxDepthOf layers)
